cba8bf9b01c7fb889045a9034f9dd7299e12809e
musmana <shinymusmana@gmail.com>
2025-10-30 21:09:31 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/address-form-and-validation/',
-  build: {
-    outDir: 'dist',
-    sourcemap: false
-  }
+  base: '/address-form-and-validation/', // 👈 must match your repo name exactly
 })
